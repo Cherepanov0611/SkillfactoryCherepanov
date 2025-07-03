62506c91e09fbf87a0ae6fd98f1813e70505b47e
Ivan Cherepanov <ivancherepanov0611@gmail.com>
2025-07-03 22:10:47 +0300

    Create mnojestvo.py
    Создайте множество цифр от 0 до 9. Напишите программу, которая принимает на вход целое число. Если это число есть в созданном множестве — программа удаляет число из множества, если числа нет в множестве — добавляет его в множество. После этого выводиться длина полученного множества и само множество.

---
+++ mnojestvo.py
@@ -0,0 +1,12 @@
+numbers = set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+            num = int(input('Введите число: '))
+            if num in numbers:
+                print('Удаляем число из множества')
+                numbers.remove(num)
+            else:
+                print('Добавляем число в множество')
+                numbers.add(num)
+            
+            print(f'Длина множества: {len(numbers)}')
+            print(numbers)
